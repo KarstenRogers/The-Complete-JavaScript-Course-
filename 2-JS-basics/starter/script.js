ce94546ce9ca6eaf1c4361a9cecec7eee88318e1
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-05-02 13:35:27 -0600

    if else statements

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -83,7 +83,7 @@ console.log(typeof x);
 /*******************************
  *Operator Precedence
  */
-
+/*
 var now = 2018;
 var yearJohn = 1989;
 var fullAge = 18;
@@ -100,6 +100,44 @@ console.log(average);
 var x, y;
 x = y = (3 + 5) * 4 - 6;
 console.log(x, y);
+ */
+
+
+/*******************************
+ *If / else statements
+ */
+
+var firstName = 'John';
+var civilStatus = 'single';
+
+if (civilStatus === 'married') {
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully marry soon :(');
+}
+
+var isMarried = true;
+if (isMarried) {
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully marry soon :(');
+}
+
+var massMark = 78; // kg
+var heightMark = 1.69; //meters
+
+var massJohn = 92; // kg
+var heightJohn = 1.95; //meters
+
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+
+if (BMIMark > BMIJohn) {
+    console.log('Mark\'s BMI is higher than John\'s.');
+} else {
+    console.log('John\'s BMI is higher than Mark\'s.');
+}
+
 
 
 
