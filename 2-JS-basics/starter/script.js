d2b604c3ea468262aba8e70c97d5ccdff5303793
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-05-02 15:34:32 -0600

    Arrays

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -240,7 +240,7 @@ yearsUntilRetirement(1965,'Kim');
 /*******************************
  *Function Statements and Expressions
  */
-
+/*
 var whatDoYouDo = function (job, firstName) {
     switch (job) {
         case 'teacher' :
@@ -257,6 +257,40 @@ var whatDoYouDo = function (job, firstName) {
 console.log(whatDoYouDo('designer', 'John'));
 console.log(whatDoYouDo('stay home', 'Jane'));
 console.log(whatDoYouDo('driver', 'Mark'));
+ */
+/*******************************
+ *Arrays
+ */
+
+//Initialize new array
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[2]);
+console.log(names.length);
+
+//Mutate array data
+names[1] = 'Ben';
+names[names.length] = 'Mary';
+console.log(names);
+
+//different data types
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+john.push('blue');
+john.unshift('Mr.');
+console.log(john);
+
+john.pop();
+john.pop();
+john.shift();
+console.log(john);
+
+console.log(john.indexOf(23));
+
+var isDesigner = john.indexOf('designer') === -1 ?
+    'John is NOT a designer' : 'John IS a designer';
+console.log(isDesigner);
 
 
 
