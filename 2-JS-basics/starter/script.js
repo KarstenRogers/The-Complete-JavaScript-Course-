2a811891d0c124c24b9e61a8f7ca4ac9c5a1602c
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-05-02 10:56:38 -0600

    Variable Mutation and Type Coercion

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,3 +1,7 @@
+/*******************************
+*Variables and data types
+*/
+/*
 var firstName = 'John';
 console.log(firstName);
 
@@ -13,4 +17,33 @@ console.log(job);
 job = 'teacher';
 console.log(job);
 
+// Variable naming rules
 var $3years = 3;
+ */
+
+/*******************************
+ *Variables mutation and type coercion
+ */
+
+var firstName = 'John';
+var age = 28;
+
+console.log(firstName + '' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + 'is a' + age + 'year old' + job + '.Is he Married?' + isMarried);
+
+/*******************************
+ *Variables mutation
+ */
+
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + 'is a' + age + 'year old' + job + '.Is he Married?' + isMarried);
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + '' + lastName);
