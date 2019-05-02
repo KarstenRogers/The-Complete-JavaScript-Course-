9527727f1f9a01ee7cb31d5d88db0eb1dd181a90
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-05-02 11:42:13 -0600

    Operator Precedence

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -53,6 +53,7 @@ console.log(firstName + '' + lastName);
 /*******************************
  *Basic Operators
  */
+/*
 var year, yearJohn, yearMark;
  now = 2018;
  ageJohn = 28;
@@ -77,6 +78,29 @@ console.log(typeof ageJohn);
 console.log(typeof 'Mark is older than John');
 var x;
 console.log(typeof x);
+ */
+
+/*******************************
+ *Operator Precedence
+ */
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+var isFullAge = now - yearJohn >= fullAge;
+console.log(isFullAge);
+
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+//Multiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6;
+console.log(x, y);
+
 
 
 
