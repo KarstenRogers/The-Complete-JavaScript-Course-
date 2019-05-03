0cd4d1ba204312701b4aba3ba472edaea4188596
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-05-03 10:23:10 -0600

    Events and Event Handling: Rolling the Dice

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -13,8 +13,21 @@ scrores = [0,0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    //1. Random Number
+    dice = Math.floor(Math.random() * 6) + 1;
+
+    //2. Display Result
+    var diceDOM = document.querySelector('.dice')
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice +'.png';
+
+    //3. Update the round score IF the rolled number was NOT 1
+})
