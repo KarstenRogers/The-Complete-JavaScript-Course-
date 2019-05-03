f0cc59bd8acc42c83086e1f98b0307d3c2287e15
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-05-03 09:00:03 -0600

    Hoisting in Practice

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,14 +1,24 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+function calculateAge(year) {
+    console.log(2016 - year);
+}
 
+var retirement = function (year) {
+    console.log(65 - (2016 - year));
+}
 
+console.log(age);
+var age = 23;
 
-
-
-
-
-
+function foo() {
+    console.log(age);
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
 
 
