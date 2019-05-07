b6d5a44cf244c51ed7c317f45c70efc5e2c58ef6
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-05-07 15:20:29 -0600

    Arrow Functions: Lexical 'this' Keyword

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -79,6 +79,7 @@ console.log(n.endsWith('sm'));
 
 //////////////////////////////////////////////////
 // Arrow Functions: Basics
+/*
 const years = [1998, 1965, 1969, 1974];
 
 let ages6 = years.map(el => 2016 - el);
@@ -90,9 +91,64 @@ console.log(ages6);
 
 ages6 = years.map((el, index) => {
     const now = new
-        Date().getFullYear();
+    Date().getFullYear();
     const age = now - el;
     return `Age element ${index + 1}:
     ${age}.`
 });
 console.log(ages6);
+ */
+
+
+//////////////////////////////////////////////////
+// Arrow Functions: Lexical 'this' Keyword
+
+//ES5
+
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function () {
+
+        var self = this;
+        document.querySelector('.green').addEventListener(
+            'click', function () {
+                var str = 'this is box number' + self.position + ' and it is' +
+                    self.color;
+                alert(str);
+            }
+        )
+    }
+};
+box5.clickMe();
+
+
+//ES6
+const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function () {
+
+        document.querySelector('.green').addEventListener(
+            'click', () => {
+                var str = 'this is box number' + this.position + ' and it is' +
+                    this.color;
+                alert(str);
+            }
+        )
+    }
+};
+box6.clickMe();
+
+function Person(name) {
+    this.name = name;
+}
+
+//ES5
+Person.prototype.myFriends5 =
+    function (friends) {
+        var arr = friends.map(function (el) {
+            return this.name + ' is friends with' + el;
+        });
+        console.log(arr);
+}
