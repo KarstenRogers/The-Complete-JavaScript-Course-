9a94122422f8ff978a8ea626b4e2340e48dc4a2a
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-05-07 09:04:02 -0600

    Updating the Budget: UI Controller

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -97,7 +97,11 @@ var UIController = (function () {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     };
 
     return {
@@ -143,6 +147,21 @@ var UIController = (function () {
           fieldsArr[0].focus();
         },
 
+        displayBudget: function (obj) {
+
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+
+            if (obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+            }
+
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -172,7 +191,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         //2. Return the budget
         var budget = budgetCtrl.getBudget();
         //3. display the budget on the UI
-        console.log(budget);
+        UICtrl.displayBudget(budget);
     };
 
     var ctrlAddItem = function() {
@@ -197,7 +216,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     return {
         init: function() {
-            console.log('Application has started')
+            console.log('Application has started');
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
             setupEvenListeners();
         }
     };
