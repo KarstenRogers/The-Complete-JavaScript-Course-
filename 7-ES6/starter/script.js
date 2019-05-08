bf8dccef091ce3a2df1f201e0c1c2b91e6e0827a
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-05-08 08:28:03 -0600

    The Spread Operator

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -205,6 +205,7 @@ console.log(retirement);
 const boxes = document.querySelectorAll('.box');
 
 //ES5
+/*
 var boxesArr5 = Array.prototype.slice.call(boxes);
 boxesArr5.forEach(function(cur) {
     cur.style.backgroundColor = 'dodgerblue';
@@ -253,5 +254,29 @@ console.log(ages[full.indexOf(true)]);
 //ES6
 console.log(ages.findIndex(cur => cur >= 18));
 console.log(ages.find(cur => cur >= 18));
+ */
 
 
+/////////////////////////////////
+// Lecture: The Spread Operator
+
+function addFourAges(a, b, c, d) {
+    return a + b + c + d;
+}
+
+var sum1 = addFourAges(18, 30, 12, 21);
+console.log(sum1);
+
+//ES5
+var ages = [18, 30, 12, 21];
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2);
+
+//ES6
+const sum3 = addFourAges(...ages);
+console.log(sum3);
+
+const familySmith = ['John', 'Jane', 'Mark'];
+const familyMiller = ['Mary', 'Bob', 'Ann'];
+const bigFamily = [...familySmith, ...familyMiller];
+console.log(bigFamily);
