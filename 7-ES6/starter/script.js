3d341a6aabc83d4ebaa40b35e191c22df2115a14
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-05-08 08:59:17 -0600

    Rest Parameters

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -259,7 +259,7 @@ console.log(ages.find(cur => cur >= 18));
 
 /////////////////////////////////
 // Lecture: The Spread Operator
-
+/*
 function addFourAges(a, b, c, d) {
     return a + b + c + d;
 }
@@ -280,3 +280,54 @@ const familySmith = ['John', 'Jane', 'Mark'];
 const familyMiller = ['Mary', 'Bob', 'Ann'];
 const bigFamily = [...familySmith, ...familyMiller];
 console.log(bigFamily);
+ */
+
+
+
+/////////////////////////////////
+// Lecture: Rest parameters
+
+//ES5
+function isFullAge5() {
+    //console.log(arguments);
+    var argsArr = Array.prototype.slice.call(arguments);
+
+    argsArr.forEach(function(cur) {
+        console.log((2016 - cur) >= 18);
+    })
+}
+
+
+//isFullAge5(1990, 1999, 1965);
+//isFullAge5(1990, 1999, 1965, 2016, 1987);
+
+
+//ES6
+function isFullAge6(...years) {
+    years.forEach(cur => console.log( (2016 - cur) >= 18));
+}
+
+isFullAge6(1990, 1999, 1965, 2016, 1987);
+
+
+//ES5
+function isFullAge5(limit) {
+    var argsArr = Array.prototype.slice.call(arguments, 1);
+
+    argsArr.forEach(function(cur) {
+        console.log((2016 - cur) >= limit);
+    })
+}
+
+
+//isFullAge5(16, 1990, 1999, 1965);
+isFullAge5(1990, 1999, 1965, 2016, 1987);
+
+
+//ES6
+function isFullAge6(limit, ...years) {
+    years.forEach(cur => console.log( (2016 - cur) >= limit));
+}
+
+isFullAge6(16, 1990, 1999, 1965, 2016, 1987);
+
