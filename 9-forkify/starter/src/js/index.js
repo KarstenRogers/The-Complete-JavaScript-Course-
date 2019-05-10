4304353c7cfbd036b3a567fed9b7cfc6549bc38a
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-05-10 13:35:23 -0600

    api key error

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,20 +1,8 @@
-import string from './models/Search';
-
-import {add, multiply, ID} from './views/searchView';
-
-
 import axios from 'axios';
 
-asnyc function getResults(query) {
-
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-
-    const key = '7812d9a42541b351de36c88917fb5799';
-
-    const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-
+async function getResults(query) {
+    const key = '8acbb5e75aea4e134ed78cd18a287780';
+    const res = await axios(`http://food2fork.com/api/search?key=${key}&q=${query}`);
     console.log(res);
-
 }
-
-getResults('pizza');
+getResults();
