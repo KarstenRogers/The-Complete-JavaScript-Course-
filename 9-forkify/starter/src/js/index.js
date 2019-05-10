2792d5ae832f4cdba5d4ce620e47ed5fe778023f
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-05-10 14:17:20 -0600

    Making our First API Calls without proxy

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -2,7 +2,13 @@ import axios from 'axios';
 
 async function getResults(query) {
     const key = '8acbb5e75aea4e134ed78cd18a287780';
-    const res = await axios(`http://food2fork.com/api/search?key=${key}&q=${query}`);
-    console.log(res);
+    try {
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+
 }
-getResults();
+getResults('chicken');
