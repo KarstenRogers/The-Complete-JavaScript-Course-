a9cf033a1906aa7ac34914ef0203120a25bf3ee6
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-05-10 10:41:47 -0600

    Planning our Project Architecture with MVC

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,5 +1,6 @@
 import string from './models/Search';
 
 import {add, multiply, ID} from './views/searchView';
+import {add, multiply, ID} from './views/searchView';
 
 console.log(`using imported functions ${add(ID, 2)} and ${multiply(3, 5)}. ${string}`);
