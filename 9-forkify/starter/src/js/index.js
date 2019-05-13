406d625ee8e65f67f6e486b6322d842d71f97128
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-05-13 07:18:10 -0600

    Building the Search Model

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,14 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const key = '8acbb5e75aea4e134ed78cd18a287780';
-    try {
-        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch (error) {
-        alert(error);
-    }
-
-}
-getResults('chicken');
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
--- 9-forkify/starter/src/js/models/Search.js
+++ 9-forkify/starter/src/js/models/Search.js
@@ -1,1 +1,18 @@
-export default 'I am an exported string.';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+    const key = '8acbb5e75aea4e134ed78cd18a287780';
+    try {
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+        this.result = res.data.recipes;
+        // console.log(this.result);
+    } catch (error) {
+        alert(error);
+        }
+    }
+}
