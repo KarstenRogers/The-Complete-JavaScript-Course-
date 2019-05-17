79fedbec6de79dcf014ab649dafd0209585e5e60
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-05-17 11:20:10 -0600

    Building the Search Controller

--- 9-forkify/starter-2/src/js/index.js
+++ 9-forkify/starter-2/src/js/index.js
@@ -1,5 +1,36 @@
 import Search from './models/Search';
 
+/** Global State of the App
+ * - Search object
+ * - Current recipe object
+ * - shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+    // 1. Get query from view
+    const query = 'pizza';
+
+    if (query) {
+        // 2. New Search object and add to state
+        state.search = new Search(query);
+
+        // 3. prepare UI for results
+
+        // 4. Search for recipes
+        await state.search.getResults();
+
+        // 5. Render results on UI
+        console.log(state.search.result)
+    }
+};
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
 const search = new Search('pizza');
 console.log(search);
 search.getResults();
