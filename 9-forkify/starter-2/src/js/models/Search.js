f945214188cb879bd613b5f39d14898e0c287495
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-05-17 13:55:09 -0600

    Rendering an AJAX Loading Spinner

--- 9-forkify/starter-2/src/js/models/Search.js
+++ 9-forkify/starter-2/src/js/models/Search.js
@@ -6,7 +6,7 @@ export default class Search {
     }
 
      async getResults() {
-        const key = '00e0368da14481d74d39bf237197701e';
+        const key = 'c09d705a02dc13a6b5ae8983cd9190f1';
         try {
             const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
             this.result = res.data.recipes;
