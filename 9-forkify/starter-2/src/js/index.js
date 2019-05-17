ea1962e3dbeed2d9cc425a5577a6252fe7af9591
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-05-17 10:21:21 -0600

    Making our First API Calls

--- 9-forkify/starter-2/src/js/index.js
+++ 9-forkify/starter-2/src/js/index.js
@@ -1,4 +1,15 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module x = ${x}`);
+import axios from 'axios';
+
+async function getResults(query) {
+    const key = '00e0368da14481d74d39bf237197701e';
+    try {
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+}
+getResults('pizza');
+
+// 00e0368da14481d74d39bf237197701e
