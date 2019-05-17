3c942f2fe0aaa42cd12d1ca209522bd9b26d3177
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-05-17 10:50:14 -0600

    Building the Search Model

--- 9-forkify/starter-2/src/js/index.js
+++ 9-forkify/starter-2/src/js/index.js
@@ -1,15 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const key = '00e0368da14481d74d39bf237197701e';
-    try {
-        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch (error) {
-        alert(error);
-    }
-}
-getResults('pizza');
-
-// 00e0368da14481d74d39bf237197701e
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
